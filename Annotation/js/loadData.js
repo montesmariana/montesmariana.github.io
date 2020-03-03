9f40a353f0e449dbe48e6d4d14211496d263ee76
Mariana Montes <mariana.montes@kuleuven.be>
2020-03-03 11:54:20 +0100

    Annotation for Stefano

--- Annotation/js/loadData.js
+++ Annotation/js/loadData.js
@@ -1,11 +1,11 @@
 function loadData() {
     Promise.all([
         d3.tsv("church.tsv"),
-        d3.json("church.json"),
-        d3.json("users.json"),
+        d3.json("senses.json"),
+        d3.json("concept.json"),
         d3.json("messages.json")
     ]).then(function(files) {
         // console.log(JSON.parse(files[1]));
-        execute(concordance = files[0], definitions = files[1], users = files[2], messages = files[3]);
+        execute(concordance = files[0], category1 = files[1], category2 = files[2], messages = files[3]);
     });
 }
