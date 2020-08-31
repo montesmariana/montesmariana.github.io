34a89182fa432c44638e670b1390b26c5dabbdf8
Mariana Montes <mariana.montes@kuleuven.be>
2020-08-31 15:14:37 +0200

    add non-selection to cwsTable
    distinguish between + columns (with data on selected tokens) and -columns (data on non selected tokens)

--- NephoVis/src/cwsTable.js
+++ NephoVis/src/cwsTable.js
@@ -3,9 +3,8 @@ function execute(datasets, type) {
     const modelSelection = listFromLS("modelselection-" + type);
     // const modelSelection = listFromLS("modelselection-" + type + "-group" + group);
     const tokSelection = listFromLS("tokenselection-" + type);
-    const bigdata = datasets["variables"].filter((t) => {
-        return (tokSelection.indexOf(t["_id"]) !== -1);
-    });
+    const selectedTokens = datasets["variables"].filter((t) => tokSelection.indexOf(t["_id"]) !== -1 );
+    const deselectedTokens = datasets["variables"].filter((t) => tokSelection.indexOf(t["_id"]) === -1 );
     function findCwsColumn(m) {
         return (datasets["variables"].columns.filter((d) => {
             return (d.startsWith("_cws.") && m.search(d.slice(5)) === 0)
@@ -14,18 +13,22 @@ function execute(datasets, type) {
     const cwsColumns = modelSelection.map(findCwsColumn);
 
     const cws = cwsColumns.map((m) => { // for each model
-        return (bigdata.map((d) => { return (d[m]); }).join(";"));
+        return (selectedTokens.map((d) => { return (d[m]); }).join(";"));
     }).join(";").split(";").filter((d) => {return(d !== "NA"); });
 
-    const freqcols = modelSelection.map((d, i) => { return (i + 1); });
+    const freqcols = modelSelection.map((d) => modelSelection.indexOf(d) + 1 );
 
-    const cols = ["CWs", "totalFreq", ...freqcols];
-
+    const cols = ["CWs", "total+", "total-"];
+    freqcols.forEach((d) => {
+        cols.push(d + "+");
+        cols.push(d + "-");
+    });
 
     const tableData = _.uniq(cws).map((d) => {
-        const start = { "CWs": d, "totalFreq": countTokens(d) };
+        const start = { "CWs": d, "total+": countTokens(d, selectedTokens), "total-" : countTokens(d, deselectedTokens) };
         for (let i = 0; i < freqcols.length; i++) {
-            start[freqcols[i]] = countCws(cwsColumns[i], d);
+            start[freqcols[i] + "+"] = countCws(cwsColumns[i], d, selectedTokens);
+            start[freqcols[i] + "-"] = countCws(cwsColumns[i], d, deselectedTokens);
         }
         return (start);
     });
@@ -39,39 +42,38 @@ function execute(datasets, type) {
 
     const rows = table.append('tbody').selectAll('tr')
         .data(tableData).enter()
-        .append('tr');
+        .append('tr')        
+    
     rows.selectAll('td')
-        .data((d) => {
-            return (cols.map(function (k) {
-                return ({ 'name': k, 'value': d[k] });
-            }));
-        }).enter()
+        .data((d) => cols.map((k) => {return({ 'name': k, 'value': d[k] })}))
+            .enter()
         .append('td')
+        .attr("class", d => d.name.endsWith("+") ? "plus" : "minus")
         .attr('data-th', d => d.name)
         .text(d => d.value);
 
     $("table").DataTable({
         // 'destroy' : true,
-        "order": [[1, "desc"]],
+        "order": [[1, "desc"]]
         // "columnDefs": [{ "visible": false, "targets": 1 }]
     });
 
-    function countTokens(cw) {
+    function countTokens(cw, df) {
         let res = []
         for (let i = 0; i < cwsColumns.length; i++){
             const m = cwsColumns[i];
-            const tids = bigdata.filter((t) => {
-                return(t[m].split(";").indexOf(cw) !== -1);
-            }).map((t) => { return(t["_id"]); });
+            const tids = df
+                .filter((t) => t[m].split(";").indexOf(cw) !== -1 )
+                .map((t) => t["_id"] );
             res.push(...tids);
         }
         return(_.uniq(res).length);
     }
 
-    function countCws(m, cw) {
-        const res = bigdata.map((d) => { return (d[m]); })
+    function countCws(m, cw, df) {
+        const res = df.map((d) => d[m])
             .join(";").split(";")
-            .filter((w) => { return (w === cw); });
+            .filter((w) => w === cw );
         return (res.length);
     }
 }
--- NephoVis/src/level2.js
+++ NephoVis/src/level2.js
@@ -39,7 +39,7 @@ function execute(datasets, type, alternatives) {
   });
 
   d3.select("#showTable").on("click", function () {
-    const params = "width=700,height=700,menubar=no,toolbar=no,location=no,status=no";
+    const params = "width=1000,height=700,menubar=no,toolbar=no,location=no,status=no";
     window.open("cwsTable.html?type=" + type, "freqtable", params);
     // window.open("cwsTable.html?type=" + type + "&group=" + group, "freqtable", params);
   });
